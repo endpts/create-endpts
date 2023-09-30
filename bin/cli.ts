723ddca108731ad4be13feb64387889dad0ca408
Fady Abdelmalik <fady@fadymak.com>
2023-09-30 13:30:12 +0200

    feat: augment existing projects with endpts API routes

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,14 +1,46 @@
 #!/usr/bin/env node
 
-import path from "path";
+import { parseArgs } from "node:util";
 import { CreateCommand } from "../src/create.js";
+import { getPkgManager } from "../src/get-pkg-manager.js";
 
-const runtime = path.basename(process.argv[0]).replace(".exe", "");
-const _argv = process.argv.slice(2);
+const currVersion = process.versions.node;
+const currMajorVersion = parseInt(currVersion.split(".")[0], 10);
+const minMajorVersion = 18;
+
+if (currMajorVersion < minMajorVersion) {
+  console.error(
+    `The version of Node.js you are using (v${currVersion}) is not supported. Please use Node.js v${minMajorVersion} or higher.`
+  );
+  process.exit(1);
+}
+
+const packageManager = getPkgManager();
+
+const { values, positionals } = parseArgs({
+  options: {
+    existing: {
+      type: "boolean",
+    },
+    positionals: {
+      type: "string",
+      multiple: true,
+    },
+  },
+  allowPositionals: true,
+});
+
+if (values.existing && positionals.length !== 0) {
+  console.error(
+    `The --existing option cannot be used with any other arguments.`
+  );
+  process.exit(1);
+}
 
 const init = new CreateCommand({
-  name: _argv[0],
-  packageManager: runtime === "bun" ? "bun" : "npm",
+  name: positionals[0],
+  packageManager,
+  existing: values.existing,
 });
 
 await init.run();
--- src/create.ts
+++ src/create.ts
@@ -6,15 +6,18 @@ import { execa } from "execa";
 import ora from "ora";
 import chalk from "chalk";
 
-type PackageManager = "npm" | "bun";
+import { PackageManager } from "./get-pkg-manager.js";
+
 interface CreateOptions {
   name: string;
   packageManager: PackageManager;
+  existing?: boolean;
 }
 
 export class CreateCommand {
   private readonly name: string;
   private readonly packageManager: PackageManager;
+  private readonly existing?: boolean;
   private readonly projectRootDir: string;
   private readonly templatesRootDir = path.join(
     path.dirname(url.fileURLToPath(import.meta.url)),
@@ -24,10 +27,16 @@ export class CreateCommand {
   constructor(opts: CreateOptions) {
     this.name = opts.name || "my-api";
     this.packageManager = opts.packageManager;
-    this.projectRootDir = path.join(process.cwd(), this.name);
+    this.existing = opts.existing;
+
+    // if we're augmenting an existing project, the CWD is the project root
+    // otherwise, we create a new directory for the project
+    this.projectRootDir = this.existing
+      ? process.cwd()
+      : path.join(process.cwd(), this.name);
   }
 
-  createProjectDir() {
+  private createProjectDir() {
     const spinner = ora({
       text: `Creating project directory: ${this.name}`,
     }).start();
@@ -52,7 +61,7 @@ export class CreateCommand {
     }
   }
 
-  writePackageJson() {
+  private writePackageJson() {
     const spinner = ora({
       text: `Writing package.json`,
     }).start();
@@ -81,7 +90,7 @@ export class CreateCommand {
     }
   }
 
-  copyTemplateFiles(src: string, dest: string) {
+  private copyTemplateFiles(src: string, dest: string) {
     const spinner = ora({
       text: `Copying project files`,
     }).start();
@@ -117,7 +126,7 @@ export class CreateCommand {
     }
   }
 
-  async installDependencies() {
+  private async installDependencies() {
     const spinner = ora({
       text: "Installing dependencies",
     }).start();
@@ -129,27 +138,12 @@ export class CreateCommand {
       "@endpts/devtools",
     ];
 
-    const npmArgs = [
-      "install",
-      "--prefix",
-      this.projectRootDir,
-      "--save-dev",
-      ...devDeps,
-    ];
-
-    const bunArgs = [
-      "add",
-      "--cwd",
-      this.projectRootDir,
-      "--development",
-      ...devDeps,
-    ];
+    const args = ["install", "--D", ...devDeps];
 
     try {
-      await execa(
-        this.packageManager,
-        this.packageManager === "bun" ? bunArgs : npmArgs
-      );
+      await execa(this.packageManager, args, {
+        cwd: this.projectRootDir,
+      });
       spinner.succeed();
     } catch (e: any) {
       spinner.fail("Failed to install dependencies");
@@ -158,7 +152,178 @@ export class CreateCommand {
     }
   }
 
-  async run() {
+  private async createSampleRoutesDirectory({ dest }: { dest: string }) {
+    const spinner = ora({
+      text: `Creating routes directory`,
+    }).start();
+
+    // ensure the project does not already have a routes directory (we don't want to overwrite it)
+    if (fs.existsSync(path.join(dest, "routes"))) {
+      spinner.fail("A routes directory already exists in this project");
+      process.exit(1);
+    }
+
+    try {
+      fs.mkdirSync(path.join(dest, "routes"));
+      fs.writeFileSync(
+        path.join(dest, "routes/README.md"),
+        `# The routes directory
+
+This is the routes directory -- it contains all of your API routes.
+
+## Creating a new route
+
+To create a new route, simply create a new file in this directory. The file should export a default object with the following properties:
+
+- \`method\`: The HTTP method for this route (e.g. \`GET\`, \`POST\`, \`PUT\`, \`DELETE\`, etc.)
+- \`path\`: The path for this route (e.g. \`/users\`)
+- \`handler\`: An asynchronous function that handles the request and returns a response
+
+Here's an example route:
+
+\`\`\`ts
+import type { Route } from "@endpts/types";
+
+export default {
+  method: "GET",
+  path: "/users",
+  async handler(req) {
+    return Response.json([
+      { id: 1, name: "Alice" },
+      { id: 2, name: "Bob" },
+      { id: 3, name: "Charlie" },
+    ]);
+  },
+} satisfies Route;
+\`\`\`
+
+## Dev server
+
+To start the dev server, run:
+
+\`\`\`sh
+npm run dev
+\`\`\`
+
+The dev server will automatically reload when you make changes to your API routes.
+
+## Making requests to your API
+
+You can make requests to your API using fetch or any other HTTP client. For example, to make a request to the \`/users\` route, you can run:
+
+\`\`\`ts
+// ensure that you specify the correct port
+const res = await fetch("http://localhost:3000/users");
+const users = await res.json();
+\`\`\`
+`
+      );
+      fs.writeFileSync(
+        path.join(dest, "routes/get-users.ts"),
+        `import type { Route } from "@endpts/types";
+
+export default {
+  method: "GET",
+  path: "/users",
+  async handler(req) {
+    return Response.json([
+      { id: 1, name: "Alice" },
+      { id: 2, name: "Bob" },
+      { id: 3, name: "Charlie" },
+    ]);
+  },
+} satisfies Route;
+`
+      );
+
+      spinner.succeed();
+    } catch (e) {
+      spinner.fail("Failed to create routes directory");
+      console.log(e);
+      process.exit(1);
+    }
+  }
+
+  private async addDevServerScript() {
+    const spinner = ora({
+      text: "Adding dev:server script to package.json",
+    }).start();
+
+    try {
+      const packageJson = JSON.parse(
+        fs.readFileSync(path.join(this.projectRootDir, "package.json"), "utf-8")
+      );
+
+      packageJson.scripts = {
+        ...packageJson.scripts,
+        "dev:server": "endpts dev",
+      };
+
+      fs.writeFileSync(
+        path.join(this.projectRootDir, "package.json"),
+        JSON.stringify(packageJson, null, 2)
+      );
+      spinner.succeed();
+    } catch (e: any) {
+      spinner.fail("Failed to add dev:server script to package.json");
+      console.log(e.stderr || e);
+      process.exit(1);
+    }
+  }
+
+  private async addGitIgnoreEntries() {
+    const spinner = ora({
+      text: "Adding endpts-related entries to .gitignore",
+    }).start();
+
+    try {
+      // add endpts-related entries to .gitignore, if it exists
+      if (fs.existsSync(path.join(this.projectRootDir, ".gitignore"))) {
+        fs.appendFileSync(
+          path.join(this.projectRootDir, ".gitignore"),
+          "\n# endpts\n.ep\n.endpts\n"
+        );
+      }
+      spinner.succeed();
+    } catch (e: any) {
+      spinner.fail("Failed to add endpts-related entries to .gitignore");
+      console.log(e.stderr || e);
+      process.exit(1);
+    }
+  }
+
+  private async augmentExistingProject() {
+    console.log();
+    console.log(chalk.bold(`Updating your existing project`));
+    console.log();
+
+    if (!fs.existsSync(path.join(this.projectRootDir, "package.json"))) {
+      console.error(
+        `The current directory does not contain a package.json file.`
+      );
+      process.exit(1);
+    }
+
+    await this.installDependencies();
+    await this.addDevServerScript();
+    await this.createSampleRoutesDirectory({ dest: this.projectRootDir });
+    await this.addGitIgnoreEntries();
+
+    console.log();
+    console.log(
+      chalk.green.bold(`Your project has been updated to use endpts API\n`)
+    );
+    console.log(
+      `  - Start the dev server:`,
+      chalk.cyan.bold(`${this.packageManager} run dev:server`)
+    );
+    console.log(
+      `  - Create new routes in the ${chalk.bold.cyan("routes/")} directory`
+    );
+    console.log();
+  }
+
+  private async cleanBootstrap() {
     console.log();
     console.log(
       chalk.bold(`Bootstrapping a new endpts project`),
@@ -166,9 +331,9 @@ export class CreateCommand {
     );
     console.log();
 
-    await this.createProjectDir();
-    await this.writePackageJson();
-    await this.copyTemplateFiles(
+    this.createProjectDir();
+    this.writePackageJson();
+    this.copyTemplateFiles(
       path.join(this.templatesRootDir, "./basic/typescript"),
       this.projectRootDir
     );
@@ -192,5 +357,13 @@ export class CreateCommand {
       `  - Create new routes in the ${chalk.bold.cyan("routes/")} directory`
     );
     console.log();
+  }
+
+  async run() {
+    if (this.existing) {
+      return this.augmentExistingProject();
+    } else {
+      return this.cleanBootstrap();
+    }
   }
 }
